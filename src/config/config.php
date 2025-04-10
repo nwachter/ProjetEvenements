dadb9fbde5aeb0c9b9fd7c32d4a70560bd5f7157
nwachter <malaikaninaw@yahoo.fr>
2025-04-10 16:07:31 +0200

    fix db-password root

--- src/config/config.php
+++ src/config/config.php
@@ -14,31 +14,36 @@ try {
         }
         
         // Retrieve environment variables
-        $userName = $_ENV['APP_USER'] ?? 'default';
         $env = $_ENV['APP_ENV'] ?? 'development';
         
         // Get environment-specific configuration
         $config = $environmentVars[$env] ?? $environmentVars['development'];
         
+        // Get database username - should be from config or env var
+        $user = $config['db_user'] ?? $_ENV['MYSQL_USER'] ?? 'nina';
+        
         // Get the password from file
         $passFile = __DIR__ . '/../db/password.txt';
         $pass = file_exists($passFile) ? trim(file_get_contents($passFile)) : '';
         
-      
-    // If password file can't be read, try environment variable
-    if (empty($pass) && isset($_ENV['MYSQL_PASSWORD'])) {
-        $pass = $_ENV['MYSQL_PASSWORD'];
-    }
-    
-    $dsn = "mysql:host=$host;dbname=$db_name;charset=utf8";
-    
-    $options = [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES => false,
-    ];
-    
-    global $db;
+        // If password file can't be read, try environment variable
+        if (empty($pass) && isset($_ENV['MYSQL_PASSWORD'])) {
+            $pass = $_ENV['MYSQL_PASSWORD'];
+        }
+        
+        // Get database connection parameters
+        $host = $config['db_host'] ?? 'db';
+        $db_name = $config['db_name'] ?? 'projetevenements';
+        
+        $dsn = "mysql:host=$host;dbname=$db_name;charset=utf8";
+        
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+        ];
+        
+        global $db;
         $db = new PDO($dsn, $user, $pass, $options);
     }
 } catch (Exception $e) {
